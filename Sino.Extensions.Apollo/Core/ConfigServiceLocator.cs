05a3c9dd3d6f23e35c726fc65c7e3e64754463a1
姚正发 <976691141@qq.com>
2017-10-17 15:29:01 +0800

    完成编写

--- Sino.Extensions.Apollo/Core/ConfigServiceLocator.cs
+++ Sino.Extensions.Apollo/Core/ConfigServiceLocator.cs
@@ -1,9 +1,11 @@
 ﻿using NLog;
 using Sino.Extensions.Apollo.Configuration;
 using Sino.Extensions.Apollo.Core.Dto;
+using Sino.Extensions.Apollo.Exceptions;
 using Sino.Extensions.Apollo.Utils.Http;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading;
 
@@ -73,7 +75,50 @@ namespace Sino.Extensions.Apollo.Core
 
         private void UpdateConfigServices()
         {
+            lock(this)
+            {
+                string url = AssembleMetaServiceUrl();
 
+                var requset = new Sino.Extensions.Apollo.Utils.Http.HttpRequest(url);
+                int maxRetries = 5;
+                Exception exception = null;
+                for(int i = 0;i < maxRetries; i++)
+                {
+                    try
+                    {
+                        var response = _httpUtil.DoGet<IList<ServiceDTO>>(requset).Result;
+                        var services = response.Body;
+                        if (services == null || services.Count == 0)
+                            continue;
+                        _configServices = services;
+                        return;
+                    }
+                    catch(Exception ex)
+                    {
+                        logger.Warn(ex);
+                        exception = ex;
+                    }
+                    Thread.Sleep(1000);
+                }
+
+                throw new ApolloConfigException($"Get config services failed from {url}", exception);
+            }
+        }
+
+        private string AssembleMetaServiceUrl()
+        {
+            string domainName = _config.MetaServerDomainName;
+            string appId = _config.AppId;
+            string localIp = _config.LocalIp;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(domainName + "/services/config");
+            sb.Append("?appId=" + WebUtility.UrlEncode(appId));
+            if(!string.IsNullOrEmpty(localIp))
+            {
+                sb.Append("&ip=" + WebUtility.UrlEncode(localIp));
+            }
+
+            return sb.ToString();
         }
     }
 }
