fd39a9ea32b538b55042ed2955fd53bbe28e3c51
姚正发 <976691141@qq.com>
2017-10-18 13:37:31 +0800

    增加新的DTO模型

---
+++ Sino.Extensions.Apollo/Core/Dto/ApolloConfigNotification.cs
@@ -0,0 +1,37 @@
+﻿namespace Sino.Extensions.Apollo.Core.Dto
+{
+    public class ApolloConfigNotification
+    {
+        public string NamespaceName { get; set; }
+
+        public long NotificationId { get; set; }
+
+        public ApolloNotificationMessages Messages { get; set; }
+
+        public ApolloConfigNotification(string namespaceName, long notificationId)
+        {
+            NamespaceName = namespaceName;
+            NotificationId = notificationId;
+        }
+
+        public void AddMessage(string key, long notificationId)
+        {
+            if(Messages == null)
+            {
+                lock(this)
+                {
+                    if(Messages == null)
+                    {
+                        Messages = new ApolloNotificationMessages();
+                    }
+                }
+            }
+            Messages.Put(key, notificationId);
+        }
+
+        public override string ToString()
+        {
+            return $"ApolloConfigNotification{{namespaceName={NamespaceName},notificationId={NotificationId}}}";
+        }
+    }
+}
