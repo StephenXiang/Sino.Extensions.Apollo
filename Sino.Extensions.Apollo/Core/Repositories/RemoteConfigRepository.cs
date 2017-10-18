a03b68c6a7c29d50a8935b7887d751dcb90428b9
姚正发 <976691141@qq.com>
2017-10-18 15:47:13 +0800

    新增远程仓储

---
+++ Sino.Extensions.Apollo/Core/Repositories/RemoteConfigRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.Extensions.Apollo.Core.Repositories
+{
+    public class RemoteConfigRepository : AbstractConfigRepository
+    {
+
+    }
+}
